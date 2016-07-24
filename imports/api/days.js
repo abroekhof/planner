8916b49cc7e36a4009890d4e1e97da5505f77439
Alex Broekhof <abroekhof@gmail.com>
2016-07-23 21:32:13 -0700

    Add meals to day

--- imports/api/days.js
+++ imports/api/days.js
@@ -1,6 +1,7 @@
 import { Mongo } from 'meteor/mongo';
 import { Meteor } from 'meteor/meteor';
 import { check } from 'meteor/check';
+import { Meals } from './meals.js';
 
 class DaysCollection extends Mongo.Collection {}
 
@@ -8,8 +9,14 @@ export const Days = new DaysCollection('days');
 
 Meteor.methods({
   'days.insert'() {
-    Days.insert({
-      createdAt: new Date(),
+    Days.insert({ createdAt: new Date() }, (err, day) => {
+      ['Breakfast', 'Lunch', 'Dinner', 'Snacks'].forEach((meal) => {
+        Meals.insert({
+          day,
+          name: meal,
+          createdAt: 2,
+        });
+      });
     });
   },
   'days.remove'(dayId) {
