d0e701b4ef1a0b03f6c7cb29d2c929fa6ce6f00d
Alex Broekhof <abroekhof@gmail.com>
2016-10-05 10:08:29 -0700

    Update verification

--- imports/api/foods/foods.test.js
+++ imports/api/foods/foods.test.js
@@ -1,21 +1,23 @@
 /* eslint-env mocha */
+/* eslint-disable func-names, prefer-arrow-callback */
 
 import { resetDatabase } from 'meteor/xolvio:cleaner';
 import { Meteor } from 'meteor/meteor';
+import { Random } from 'meteor/random';
 import { Factory } from 'meteor/dburles:factory';
 import { assert } from 'meteor/practicalmeteor:chai';
 
+
 import Foods from './foods.js';
+import verifyFood from './server/methods.js';
 
 Factory.define('foods', Foods, {});
 
 if (Meteor.isServer) {
-  describe('foods', () => {
-    beforeEach(() => {
-      resetDatabase();
-    });
+  require('./server/schema.js');
 
-    it('builds correctly from factory', () => {
+  describe('foods', function foods() {
+    it('builds correctly from factory', function factoryBuild() {
       const food = Factory.create('foods', {
         name: 'test',
         calories: 100,
@@ -25,6 +27,70 @@ if (Meteor.isServer) {
         weight: 3,
       });
       assert.typeOf(food, 'object');
+    });
+
+    describe('methods', function methods() {
+      describe('foods.verify', function foodsVerify() {
+        beforeEach(function beforeEach() {
+          resetDatabase();
+        });
+
+        it('should verify equal foods', function sameFoods() {
+          const f = {
+            name: 'test',
+            calories: 100,
+            caloriesPerWeight: 33,
+            protein: 10,
+            proteinPerWeight: 3,
+            weight: 3,
+          };
+          const food = Factory.create('foods', f);
+          const newFoodId = verifyFood(Random.id(), null, f.name, f.calories, f.protein, f.weight);
+          assert.strictEqual(food._id, newFoodId);
+        });
+
+        it('should verify similar foods', function similarFoods() {
+          const f = {
+            name: 'test',
+            calories: 100,
+            caloriesPerWeight: 33,
+            protein: 10,
+            proteinPerWeight: 3,
+            weight: 3,
+          };
+          const food = Factory.create('foods', f);
+          const newFoodId = verifyFood(Random.id(), null, `${f.name}  one`, f.calories * 1.05, f.protein * 1.05, f.weight * 1.05);
+          assert.strictEqual(food._id, newFoodId);
+        });
+
+        it('should not verify when foods are different', function different() {
+          const f = {
+            name: 'Peanut Clif Bar',
+            calories: 100,
+            caloriesPerWeight: 33,
+            protein: 10,
+            proteinPerWeight: 3,
+            weight: 3,
+          };
+          const food = Factory.create('foods', f);
+          const newFoodId = verifyFood(Random.id(), null, 'Chocolate', f.calories * 1.5, f.protein * 1.5, f.weight * 1.5);
+          assert.notStrictEqual(food._id, newFoodId);
+        });
+
+        it('should not verify when brand names are the same', function brandNames() {
+          const f = {
+            name: 'Peanut Clif Bar',
+            calories: 100,
+            caloriesPerWeight: 33,
+            protein: 10,
+            proteinPerWeight: 3,
+            weight: 3,
+          };
+          const food = Factory.create('foods', f);
+          const newFoodId = verifyFood(Random.id(), null, 'Chocolate Clif Bar', f.calories, f.protein, f.weight);
+          assert.notStrictEqual(food._id, newFoodId);
+        });
+      });
     });
   });
 }
--- imports/api/foods/server/methods.js
+++ imports/api/foods/server/methods.js
@@ -1,106 +1,130 @@
 import { Meteor } from 'meteor/meteor';
 import { Match, check } from 'meteor/check';
+import { _ } from 'meteor/underscore';
 
 import Foods from '../foods.js';
 import MealFoods from '../../mealFoods/mealFoods.js';
 
+const verifyFood = (userId, foodId, name, calories, protein, weight) => {
+  if (foodId) {
+    const food = Foods.findOne(foodId);
+    if (!food) {
+      throw new Meteor.Error('foods.verify',
+        'No food found to update');
+    }
+    if (userId !== food.userId) {
+      throw new Meteor.Error('foods.verify.accessDenied',
+        'Cannot update a food that does not belong to you');
+    }
+    if (food.verified) {
+      throw new Meteor.Error('foods.verify.accessDenied',
+        'Cannot update a food that has been verified');
+    }
+  }
+  const percent = 0.02;
+  const upper = (1 + percent);
+  const lower = (1 - percent);
+  const foundFood = Foods.findOne(
+    {
+      $and: [
+        { $text: { $search: name } },
+        { calories: { $lte: calories * upper } },
+        { calories: { $gte: calories * lower } },
+        { protein: { $lte: protein * upper } },
+        { protein: { $gte: protein * lower } },
+        { weight: { $lte: weight * upper } },
+        { weight: { $gte: weight * lower } },
+        { userId: { $ne: userId } },
+      ],
+    },
+    { fields: { score: { $meta: 'textScore' } },
+    sort: { score: { $meta: 'textScore' } },
+    }
+  );
+  let foodsMatch = false;
+  if (foundFood) {
+    const foundToks = foundFood.name.toLowerCase().split(/\b\s+/);
+    const searchToks = name.toLowerCase().split(/\b\s+/);
+    const minLength = Math.min(foundToks.length, searchToks.length);
+    foodsMatch = (minLength === _.intersection(foundToks, searchToks).length);
+    console.log(foodsMatch);
+  }
+  if (foodsMatch) {
+    console.log(foundFood.score);
+    // food was found, update it to make sure it's verified
+    Foods.update(foundFood._id, { $set: { verified: true } });
+    if (foodId) {
+      // an update was requested which verified a food, so change any previous MealFoods to use
+      // the new food, and delete the old food
+      MealFoods.update(
+        { foodId },
+        { $set: {
+          foodId: foundFood._id,
+          name: foundFood.name,
+          calories: foundFood.calories,
+          caloriesPerWeight: foundFood.caloriesPerWeight,
+          protein: foundFood.protein,
+          proteinPerWeight: foundFood.proteinPerWeight,
+          weight: foundFood.weight,
+        },
+      });
+      Foods.remove(foodId);
+    }
+    return foundFood._id;
+  }
+  // no closely matching food was found
+  if (foodId) {
+    // a food update was requested
+    Foods.update(
+      foodId,
+      { $set: {
+        name,
+        calories,
+        caloriesPerWeight: calories / weight,
+        protein,
+        proteinPerWeight: protein / weight,
+        weight,
+      },
+    });
+    MealFoods.update(
+      { foodId },
+      { $set: {
+        name,
+        calories,
+        caloriesPerWeight: calories / weight,
+        protein,
+        proteinPerWeight: protein / weight,
+        weight,
+      },
+    });
+    return foodId;
+  }
+  // no matching food and no update, so just create a new food
+  return Foods.insert({
+    userId,
+    name,
+    calories,
+    caloriesPerWeight: calories / weight,
+    protein,
+    proteinPerWeight: protein / weight,
+    weight,
+  });
+};
+
 Meteor.methods({
   'foods.verify': function foodsVerify(foodId, name, calories, protein, weight) {
-    if (!this.userId) {
+    const userId = this.userId;
+    if (!userId) {
       throw new Meteor.Error('foods.verify.accessDenied',
         'Cannot create a food when not logged in');
     }
     check(foodId, Match.Maybe(String));
-    if (foodId) {
-      const food = Foods.findOne(foodId);
-      if (this.userId !== food.userId) {
-        throw new Meteor.Error('foods.remove.accessDenied',
-          'Cannot update a food that does not belong to you');
-      }
-      if (food.verified) {
-        throw new Meteor.Error('foods.remove.accessDenied',
-          'Cannot update a food that has been verified');
-      }
-    }
     check(name, String);
     check(calories, Number);
     check(protein, Number);
     check(weight, Number);
-    const foundFood = Foods.findOne(
-      {
-        $and: [
-          { $text: { $search: name } },
-          { calories: { $lte: calories * 1.05 } },
-          { calories: { $gte: calories * 0.95 } },
-          { protein: { $lte: protein * 1.05 } },
-          { protein: { $gte: protein * 0.95 } },
-          { weight: { $lte: weight * 1.05 } },
-          { weight: { $gte: weight * 0.95 } },
-          { userId: { $ne: this.userId } },
-        ],
-      },
-      { fields: { score: { $meta: 'textScore' } },
-      sort: { score: { $meta: 'textScore' } },
-      }
-    );
-    if (foundFood) {
-      // food was found, update it to make sure it's verified
-      Foods.update(foundFood._id, { $set: { verified: true } });
-      if (foodId) {
-        // an update was requested which verified a food, so change any previous MealFoods to use
-        // the new food, and delete the old food
-        MealFoods.update(
-          { foodId },
-          { $set: {
-            foodId: foundFood._id,
-            name: foundFood.name,
-            calories: foundFood.calories,
-            caloriesPerWeight: foundFood.caloriesPerWeight,
-            protein: foundFood.protein,
-            proteinPerWeight: foundFood.proteinPerWeight,
-            weight: foundFood.weight,
-          },
-        });
-        Foods.remove(foodId);
-      }
-      return foundFood._id;
-    }
-    // no closely matching food was found
-    if (foodId) {
-      // a food update was requested
-      Foods.update(
-        foodId,
-        { $set: {
-          name,
-          calories,
-          caloriesPerWeight: calories / weight,
-          protein,
-          proteinPerWeight: protein / weight,
-          weight,
-        },
-      });
-      MealFoods.update(
-        { foodId },
-        { $set: {
-          name,
-          calories,
-          caloriesPerWeight: calories / weight,
-          protein,
-          proteinPerWeight: protein / weight,
-          weight,
-        },
-      });
-      return foodId;
-    }
-    // no matching food and no update, so just create a new food
-    return Foods.insert({
-      userId: this.userId,
-      name,
-      calories,
-      caloriesPerWeight: calories / weight,
-      protein,
-      proteinPerWeight: protein / weight,
-      weight,
-    });
+    return verifyFood(userId, foodId, name, calories, protein, weight);
   },
 });
+
+export default verifyFood;
